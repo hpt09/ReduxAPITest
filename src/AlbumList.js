67cfa6fcc038b31681a2541ccbe27c92f32e29f8
Harpreet <hpt09@live.com>
2020-05-01 14:22:57 +1200

    Fixed a bug where links were not showing up since router, route also needed to be imported with Link

--- src/AlbumList.js
+++ src/AlbumList.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import axios from 'axios';
-import { BrowserRouter as Link} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import {connect } from 'react-redux';
 import DisplayAlbums from './actions/DisplayAlbums'
 
